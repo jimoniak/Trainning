a9f9fae66bc9981e8debf91b12425d6c19595001
LimbStudio <mail.limbstudio@gmail.com>
2015-01-31 20:56:59 +0100

    Ajout fonctionnalités Map

---
+++ Sources/carte.cpp
@@ -0,0 +1,61 @@
+
+#include "ressourceLoader.hpp"
+#include "carte.hpp"
+
+
+Map::Map()
+{
+
+
+
+}
+
+Map::Map(sf::Vector2f dimension,sf::RenderWindow *window)
+{
+    m_dimension = dimension;
+    m_window = window;
+
+
+}
+
+Map::~Map(){}
+
+
+
+ void Map::createMap(RessourceLoader &rsholder)
+ {
+     sf::Image   m_imageCarte;
+     sf::Image   m_imageTile;
+
+     m_imageTile = rsholder.getTexTile(0)->copyToImage();
+
+     m_imageCarte.create (m_dimension.x * m_imageTile.getSize().x,m_dimension.y * m_imageTile.getSize().y); //creation du support en memoire où blitter les tiles.
+     m_imageCarte.createMaskFromColor(sf::Color::Black);
+
+     for(unsigned int j=0; j<m_dimension.y; j++)
+    {
+        for(unsigned int i=0; i<m_dimension.x; i++)
+        {
+            m_imageCarte.copy(m_imageTile,i*m_imageTile.getSize().x,j*m_imageTile.getSize().y,sf::IntRect(0, 0, 0, 0),true); //Copie des tiles sur le support,
+
+        }
+
+        m_texture.loadFromImage(m_imageCarte);
+        m_texture.setSmooth(true);
+        m_apparence.setTexture(m_texture);
+
+
+
+
+
+    }
+
+ }
+
+ bool Map::display()
+ {
+
+     m_window->draw(m_apparence);
+     return true;
+
+ }
--- Sources/carte.hpp
+++ Sources/carte.hpp
@@ -16,18 +16,29 @@
 #define MAP
 
 #include <SFML/Graphics.hpp>
+#include "affichable.hpp"
 
-
-class Map
+class Map : public Displayable
 {
-	
+
 	private:
-	sf::Vector2f m_dimension;
-		
-	
+	sf::Vector2f m_dimension;//en nombres de tiles
+	sf::Texture  m_texture;
+	sf::Sprite    m_apparence;
+
+
 	public:
-	
-	
+	    Map();
+	    Map(sf::Vector2f dimension,sf::RenderWindow *window);
+      ~Map();
+
+
+	    void createMap(RessourceLoader &rsholder);
+
+	    bool display();
+
+
+
 };
 
 
